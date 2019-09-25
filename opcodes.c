b72733e6afd3f700983cb8bb3a47c4800e20bfca
humeinstein <humeincreations@gmail.com>
2019-09-25 19:00:16 -0400

    [INIT]pall, push, pint

--- opcodes.c
+++ opcodes.c
@@ -1,13 +1,48 @@
 #include "monty.h"
 /**
-*
+* pop - function that pops node
 *
 *
 */
-void pop(**head, unsigned int line)
+void *pop(stack_t **head, unsigned int line)
 {
     *temp = *head->next;
     free(*head);
     *head = temp;
+    return(NULL)
 }
-void push(**head, unsigned int line)
+void *push(stack_t **head, unsigned int value, unsigned int line)
+{
+    *new = *head;
+    new->n = value;
+    new->next = *head;
+    new->prev = NULL;
+
+    if ((*head) != NULL)
+        (*head)->prev = new;
+    *head = new;
+}
+void *pall(stack_t **head, unsigned int line)
+{
+    size_t inc;
+    inc = 0;
+
+    while (*head)
+    {
+        inc++
+        printf("%d\n", *head->n);
+        *head = *head->next;
+    }
+    return(NULL);
+}
+void *pint(stack_t **head, unsigned int line)
+{
+    if (*head != NULL)
+    {
+        printf("%d", *head->n);
+    }
+}
+void *swap(stack_t **head, unsgined int line)
+{
+    
+}
