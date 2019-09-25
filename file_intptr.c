68a1806d660e699e9d1f9fc0efff98224f047807
CARTER_CLEMENTS <831@holbertonschool.com>
2019-09-25 14:54:49 -0400

    Converts a file to an array for use in execution and interpretation

---
+++ file_intptr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "test_header.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+/**
+ * file_to_array - converts a file to an array
+ * @file: The file to read from
+ *
+ * Return: an array containing all contents from file
+ */
+char *file_to_array(const char *file)
+{
+	char *file_out;
+	int file_desc, file_size;
+
+	file_desc = open(file, O_RDONLY);
+
+	struct stat st;
+	stat(file, &st);
+	file_size = st.st_size;
+
+	file_out = malloc(file_size);
+
+	if (!file_out)
+		return (NULL);
+	if (file_desc == -1)
+		return (NULL);
+
+	return (file_out);
+
+}
