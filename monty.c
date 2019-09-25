581245474df3ffd7c6e71d94fd30f964bc334e21
CARTER_CLEMENTS <831@holbertonschool.com>
2019-09-25 18:52:54 -0400

    The core of the monty interpretor

--- monty.c
+++ monty.c
@@ -1,12 +1,29 @@
 #include "monty.h"
 
+/**
+ * main - The core of the monty interpretor
+ * @argc: THe amount of args
+ * @argv: The command line arguments
+ *
+ * Return: 0 if successful
+ */
 int main(int argc, char *argv[])
 {
-	char *file_input;
+	char **file_input, *line_parse;
+	int line_count, line_inc;
 
-	file_input = file_to_array(argv[1]);
+	if (argc > 1)
+	{
 
-	printf("%s", file_input);
+		file_input = file_to_array(argv[1]);
+
+		for (line_count = 0; file_input[line_count] != NULL; line_count++)
+			line_parser(file_input[line_count]);
+
+
+	}
+	else
+		printf("Error: No input file detected\n");
 
 	return (0);
 }
