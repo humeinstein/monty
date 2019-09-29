817f0d54996eb10c48be619986cfc6fe645d83ae
humeinstein <humeincreations@gmail.com>
2019-09-29 18:11:42 -0400

    [INIT]void nop - does nothing

---
+++ opcodes_2.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+* nope - function that does nothing
+* @head: pointer to head
+* @line: line command is being called from file
+* 
+*/
+void nope(stack_t **head, unsigned int line)
+{
+    *head = *head;
+    line = line;
+}
